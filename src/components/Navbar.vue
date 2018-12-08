12e32e1921c8f093a74266a874d90850a4e552ce
Askarbek Karasasev <askar.karasaev@gmail.com>
2018-12-09 00:47:59 +0900

    Adding Smoothie Component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,11 +2,11 @@
     <div class="navbar">
         <nav class="nav-extended indigo darken-2">
             <div class="nav-content">
-                <router-link to="">
+                <router-link :to="{ name: 'Index' }">
                 <span class="nav-title">Ninja Smoothies</span>
             </router-link>
             <a href="" class="btn-floating btn-large halfway-fab pink">
-                <router-link to="">
+                <router-link :to="{ name: 'AddSmoothie' }">
                     <i class="material-icons">add</i>
                 </router-link>
             </a>
@@ -20,7 +20,7 @@ export default {
     name: 'Navbar',
     data(){
         return{
-            
+
         }
     }
 }
